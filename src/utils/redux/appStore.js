b0fde6056e40c66fdfd1f2011f2556d5fcf20174
sharafhamza <kingsharaf14@gmail.com>
2025-06-08 23:53:59 +0600

    reducer fully working

--- src/utils/redux/appStore.js
+++ src/utils/redux/appStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "./movieSlice";
 import userReducer from "./userSlice";
 const appStore = configureStore({
   reducer: {
     user: userReducer,
+    movies: moviesReducer,
   },
 });
 
